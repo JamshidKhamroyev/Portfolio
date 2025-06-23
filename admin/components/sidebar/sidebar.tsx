fdaad5bd886239dfce123f37bb1db09a1436eeb8
JamshidKhamroyev <jamshidxamroyev11@gmail.com>
2025-06-23 12:25:04 +0500

    adding responsive

--- admin/components/sidebar/sidebar.tsx
+++ admin/components/sidebar/sidebar.tsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
   const pathname = usePathname(); // <-- joriy sahifa URL
 
   return (
-    <div className="h-full p-4 bg-slate-100 dark:bg-slate-900 text-slate-900 dark:text-white flex flex-col gap-4">
+    <div className="h-full md:p-4 p-2 bg-slate-100 dark:bg-slate-900 text-slate-900 dark:text-white flex flex-col gap-4">
       <h2 className="text-xl font-bold mb-4">Menu</h2>
 
       {menuItems.map((item) => {
@@ -37,14 +37,14 @@ const Sidebar = () => {
           <Link
             key={item.href}
             href={item.href}
-            className={`flex items-center gap-2 p-2 rounded transition ${
+            className={`flex items-center gap-2 md:p-2 p-1 rounded transition ${
               isActive
                 ? "bg-blue-600 text-white"
                 : "hover:bg-slate-200 dark:hover:bg-slate-800"
             }`}
           >
             <Icon size={20} />
-            <span>{item.title}</span>
+            <span className="max-md:hidden">{item.title}</span>
           </Link>
         );
       })}
